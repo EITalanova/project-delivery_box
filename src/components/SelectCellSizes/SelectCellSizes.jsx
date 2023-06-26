ab3ace24f80909a08ac72594ca35e563368cfb98
EITalanova <elenabelichenko77@gmail.com>
2023-06-26 11:33:59 +0300

    Clear code

--- src/components/SelectCellSizes/SelectCellSizes.jsx
+++ src/components/SelectCellSizes/SelectCellSizes.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-
 import { selectCellAvailability } from '../../redux/cellAvailability/cellAvailabilitySelector';
 import { fetchCellAvailability } from '../../redux/cellAvailability/cellAvailabilityThunk';
 
@@ -19,13 +18,11 @@ export const SelectCellSizes = () => {
   const [choiceCell, setChoiceCell] = useState(null);
 
   const { cell_types } = useSelector(selectCellAvailability);
-  console.log(cell_types);
 
   useEffect(() => {
- 
     setCellAvailability(cell_types);
     setId(DEVICE_UID);
-  // eslint-disable-next-line
+    // eslint-disable-next-line
   }, [DEVICE_UID, cell_types]);
 
   useEffect(() => {
@@ -40,46 +37,55 @@ export const SelectCellSizes = () => {
     <div className={style.selectCellBox}>
       <Title text="Оберіть розмір посилки" />
 
-       {cellAvailability && (<ul className={style.cellBox}>
-        {handlerCellData(cellAvailability, sizeMappings).map(
-          ({
-            type,
-            height,
-            name,
-            icon,
-            cellWidth,
-            cellHeight,
-            width,
-            isAvailable,
-          }) => (
-            <li
-              onClick={onClick}
-              style={{
-                height: `${height}px`,
-                width: `${width}px`,
-              }}
-              className={
-                choiceCell === type
-                  ? style.cellEllIsChoice
-                  : isAvailable
-                  ? style.cellEll
-                  : style.cellEllNotAvailable
-              }
-              key={type}
-              id={type}
-            >
-              <img alt={name} src={icon} className={style.cellSvg} />
+      {cellAvailability && (
+        <ul className={style.cellBox}>
+          {handlerCellData(cellAvailability, sizeMappings).map(
+            ({
+              type,
+              height,
+              name,
+              icon,
+              cellWidth,
+              cellHeight,
+              width,
+              isAvailable,
+            }) => (
+              <li
+                onClick={onClick}
+                style={{
+                  height: `${height}px`,
+                  width: `${width}px`,
+                }}
+                className={
+                  choiceCell === type
+                    ? style.cellEllIsChoice
+                    : isAvailable
+                    ? style.cellEll
+                    : style.cellEllNotAvailable
+                }
+                key={type}
+                id={type}
+              >
+                <img
+                  alt={name}
+                  src={icon}
+                  className={style.cellSvg}
+                  id={type}
+                />
 
-              <div className={style.cellTextBox}>
-                <h3 className={style.cellTextTitle}>{name}</h3>
-                <p className={style.cellTextDiscription}>
-                  {cellWidth}x{cellHeight}см
-                </p>
-              </div>
-            </li>
-          )
-        )}
-      </ul>)}
+                <div className={style.cellTextBox} id={type}>
+                  <h3 className={style.cellTextTitle} id={type}>
+                    {name}
+                  </h3>
+                  <p className={style.cellTextDiscription} id={type}>
+                    {cellWidth}x{cellHeight}см
+                  </p>
+                </div>
+              </li>
+            )
+          )}
+        </ul>
+      )}
 
       <ButtonNavigate
         pathBtnBack={`/${DEVICE_UID}/packed`}

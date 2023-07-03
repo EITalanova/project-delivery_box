f04fbca3d5196ec230eec5ef46272a2fbdb4d820
EITalanova <elenabelichenko77@gmail.com>
2023-07-03 15:07:54 +0300

    Update configs.js

--- src/configs.js
+++ src/configs.js
@@ -1,2 +1,5 @@
-export const DEVICE_UID = process.env.REACT_APP_DEVICE_UID;
-export const BASE_URL = process.env.REACT_APP_BASE_URL;
+// export const DEVICE_UID = process.env.REACT_APP_DEVICE_UID;
+// export const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+export const DEVICE_UID = '34670a76-6022-4df5-ac1d-68f88589c904';
+export const BASE_URL = 'https://dev.hub.omnic.solutions';
